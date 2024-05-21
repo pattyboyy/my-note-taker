c8dc60f630bd70f68a9c1d75d960f465ef8733dd
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-05-20 20:22:51 -0600

    gg

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
+  res.sendFile(path.join(__dirname, 'notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // API Routes
