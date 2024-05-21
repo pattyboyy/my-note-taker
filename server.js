c303c0281b1bc361860f07f2f191bbcaf8d3aa09
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-05-20 19:50:04 -0600

    a

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
